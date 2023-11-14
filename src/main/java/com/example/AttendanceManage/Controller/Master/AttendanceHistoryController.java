0ff6fb1c2e8e5c0b2ceddcc9412cc1ed70134b95
kakine-a131 <st209211675.human21@gmail.com>
2023-11-14 16:41:19 +0900

    修正しました

--- src/main/java/com/example/AttendanceManage/Controller/Master/AttendanceHistoryController.java
+++ src/main/java/com/example/AttendanceManage/Controller/Master/AttendanceHistoryController.java
@@ -3,7 +3,7 @@ package com.example.AttendanceManage.Controller.Master;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controllercd 
+@Controller
 public class AttendanceHistoryController {
     @RequestMapping("master/history")
     private String index()
