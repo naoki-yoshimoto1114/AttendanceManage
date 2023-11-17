d84ebdde554462c0f2f87458034f2cd3e0eedafa
paperPenci1 <studying07.2023@gmail.com>
2023-11-17 17:27:31 +0900

    history.htmlでの仮データ表示（表示のみの確認/フォーマット成形・計算処理は未実装）

--- src/main/java/com/example/AttendanceManage/Controller/User/HistoryController.java
+++ src/main/java/com/example/AttendanceManage/Controller/User/HistoryController.java
@@ -1,13 +1,25 @@
 package com.example.AttendanceManage.Controller.User;
 
+import com.example.AttendanceManage.Entity.Attendance;
+import com.example.AttendanceManage.repositories.historyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class HistoryController {
+    @Autowired
+    historyRepository repository;
+
     @GetMapping("history")
-    public String index()
-    {
+    public String index(Model model){
+        List<Attendance> list = repository.findAll();
+        model.addAttribute("data", list);
         return "user/history";
     }
 }
--- src/main/java/com/example/AttendanceManage/repositories/historyRepository.java
+++ src/main/java/com/example/AttendanceManage/repositories/historyRepository.java
@@ -1,4 +1,13 @@
 package com.example.AttendanceManage.repositories;
 
-public interface historyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.AttendanceManage.Entity.Attendance;
+
+import java.util.Optional;
+
+@Repository
+public interface historyRepository extends JpaRepository<Attendance, Integer> {
+
 }
