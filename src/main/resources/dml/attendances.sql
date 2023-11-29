b86692c77ae099ab6d126736feec0cca7bc746b4
naoki-yoshimoto1114 <n.yoshimoto1114@gmail.com>
2023-11-29 11:01:12 +0900

    attendancesテーブルにデータを挿入するsqlを追加

---
+++ src/main/resources/dml/attendances.sql
@@ -0,0 +1,3 @@
+INSERT INTO attendances (user_id, begin_time, end_time, rest_start, rest_end, place, date, status, rest_time, working_time) VALUES
+  ('90001', '09:00:00', '18:00:00', '12:00:00', '13:00:00', 'Office A', '2023-11-29', '勤務中', null, null),
+  ('90002', '08:30:00', '17:30:00', '12:30:00', '13:30:00', 'Office B', '2023-11-29', '未出勤', null, null),
