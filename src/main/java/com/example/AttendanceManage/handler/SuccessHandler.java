40da5752feca699130432c61344f679cdb4a2225
naoki-yoshimoto1114 <n.yoshimoto1114@gmail.com>
2023-12-04 11:10:03 +0900

    ログイン時にユーザ名とユーザIDをセッションに保存

---
+++ src/main/java/com/example/AttendanceManage/handler/SuccessHandler.java
@@ -0,0 +1,40 @@
+package com.example.AttendanceManage.handler;
+
+import com.example.AttendanceManage.Entity.User;
+import com.example.AttendanceManage.repositories.UserCrudRepository;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+public class SuccessHandler implements AuthenticationSuccessHandler
+{
+    @Autowired
+    private UserCrudRepository userCrudRepository;
+
+    @Autowired
+    private HttpSession session;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException
+    {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String userId = userDetails.getUsername();
+
+        Optional<User> loginUser = userCrudRepository.findByUserId(userId);
+
+        session.setAttribute("userId", loginUser.get().getId());
+        session.setAttribute("name", loginUser.get().getName());
+
+        response.sendRedirect("/");
+    }
+}
