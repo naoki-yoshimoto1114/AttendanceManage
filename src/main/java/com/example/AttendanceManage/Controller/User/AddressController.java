6dc7d9cdddd0ff59f0b029f10cb08c6a263c9a65
shigemoto <tsubasashigemoto@gmail.com>
2023-12-18 12:13:32 +0900

    連絡先登録の作成

--- src/main/java/com/example/AttendanceManage/Controller/User/AddressController.java
+++ src/main/java/com/example/AttendanceManage/Controller/User/AddressController.java
@@ -1,13 +1,34 @@
 package com.example.AttendanceManage.Controller.User;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddressController {
-    @GetMapping("address")
-    public String index()
-    {
+        @Autowired
+        private JdbcTemplate jdbcTemplate;
+        private HttpSession session;
+        @RequestMapping("/address")
+        private String getContact(){return "user/address";}
+        @PostMapping("/Contact")
+        public String addContact(
+            @RequestParam String email,
+            @RequestParam String tel,
+            @RequestParam String note){
+        Integer attendance_id = (Integer)session.getAttribute("attendance_id");
+        String sql = "UPDATE address SET tel = ?, email = ?, note = ? WHERE attendance_id = ?";
+        try {
+                jdbcTemplate.update(sql, email, tel, note);
+        }catch (Exception e) {
+                e.printStackTrace();
+        }
         return "user/address";
     }
 }
